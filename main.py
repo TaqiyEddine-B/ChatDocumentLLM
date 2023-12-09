6d6b7543bac9196bbe33ffdf47011628243f92b9
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2023-12-09 09:19:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import os
+import sys
+
+import pinecone
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Pinecone
+import streamlit as st
+load_dotenv()
+
+def do():
+    pinecone.init(api_key=os.environ['PINECONE_API'], environment='gcp-starter')
+
+    # Load and preprocess the PDF document
+    loader = PyPDFLoader('./CV_BOUKLOUHA.pdf')
+    documents = loader.load()
+
+    # Split the documents into smaller chunks for processing
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+
+
+    # Use HuggingFace embeddings for transforming text into numerical vectors
+    embeddings = HuggingFaceEmbeddings()
+
+    # Set up the Pinecone vector database
+    index_name = "index"
+    index = pinecone.Index(index_name)
+    vectordb = Pinecone.from_documents(texts, embeddings, index_name=index_name)
+
+    llm = OpenAI()
+
+    return llm, vectordb
+
+llm, vectordb = do()
+# Set up the Conversational Retrieval Chain
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm,
+    vectordb.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True
+)
+chat_history=[]
+
+def old():
+    query = input('Prompt: ')
+    if query.lower() in ["exit", "quit", "q"]:
+        print('Exiting')
+        sys.exit()
+    result = qa_chain({'question': query, 'chat_history': chat_history})
+    print('Answer: ' + result['answer'] + '\n')
+    chat_history.append((query, result['answer']))
+
+
+
+
+def send_message_to_llm_api(messages):
+
+    # response= make_post_request(url=llm_api_url,params=params,debug =False)
+    # response = requests.post(url, headers=headers, data=json.dumps({"messages": messages}))
+    # if response.status_code == 200:
+    #     data= response.json()
+    #     return data['choices'][0]['message']['content']
+    # else:
+    #     print(f"Error {response.status_code}: {response.text}")
+    #     return None
+    query = messages[-1]['content']
+
+    result = qa_chain({'question': query, 'chat_history': chat_history})
+    chat_history.append((query, result['answer']))
+
+    return result['answer']
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
+if prompt := st.chat_input():
+
+    message = [{"role": "user", "content":str(prompt)}]
+    st.session_state.messages.append(message[0])
+    st.chat_message("user").write(prompt)
+
+    response = send_message_to_llm_api(st.session_state.messages)
+    response_ = [{"role": "assistant", "content":str(response)}]
+
+    st.session_state.messages.append(response_[0])
+    st.chat_message("assistant").write(response)
