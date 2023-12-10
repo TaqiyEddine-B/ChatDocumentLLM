4671414c1b8f350a6797decbba72d948f77726b4
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2023-12-10 09:00:23 +0100

    Save the uploaded file locally

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 """ Main file for the chatbot. """
 from dotenv import load_dotenv
 
@@ -9,16 +11,20 @@ from src.chat_agent import ChatAgent
 from src.chroma_ import ChromaPy
 from src.utils import chat_bot
 
-
-
 st.write("# Chat with your documents using langchain and streamlit")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     string_data = bytes_data.decode("utf-8")
+
+    # Save the file in the current directory
+    file_path = os.path.join(os.getcwd(), 'my_file.txt')
+    with open(file_path, "w") as file:
+        file.write(string_data)
+    st.write("File saved in: ", file_path)
     ch = ChromaPy()
-    ch.prepare(txt_file=uploaded_file.name)
+    ch.prepare(txt_file=file_path)
     with st.expander("File content"):
         st.write(string_data)
     chat_bot = ChatAgent(fun=ch.chat_function)
