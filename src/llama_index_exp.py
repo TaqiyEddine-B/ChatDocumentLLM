d315d3ebe26305cad6d992e7c775086e8c1ace8e
TaqiyEddine Bouklouha <25593465+TaqiyEddine-B@users.noreply.github.com>
2024-10-13 06:52:11 +0000

    Refactor LlamaIndexQuery class to use OpenAI chat functionality

--- src/llama_index_exp.py
+++ src/llama_index_exp.py
@@ -4,8 +4,10 @@ import os
 # used by llama_index 
 import nltk
 nltk.download('stopwords')
-
+import streamlit as st
 from llama_index.core  import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.llms.openai import OpenAI
+from llama_index.core.memory import ChatMemoryBuffer
 
 
 class LlamaIndexQuery:
@@ -15,15 +17,30 @@ class LlamaIndexQuery:
 
         if not os.getenv('OPENAI_API_KEY'):
             os.environ['OPENAI_API_KEY'] = openai_key
+        self.llm = OpenAI(model="gpt-4")
 
     def prepare(self, ):
         """ Prepare the index for the query engine."""
+        
         documents = SimpleDirectoryReader("data").load_data()
         index = VectorStoreIndex.from_documents(documents)
-        self.engine = index.as_query_engine()
+        memory = ChatMemoryBuffer.from_defaults(token_limit=3900)
 
+        self.engine = index.as_chat_engine(
+            chat_mode="condense_plus_context",
+            memory=memory,
+            llm=self.llm,
+            context_prompt=(
+                "You are a chatbot, able to have normal interactions, as well as talk"
+                " about the document content. "
+                "Here are the relevant documents for the context:\n"
+                "{context_str}"
+                "\nInstruction: Use the previous chat history, or the context above, to interact and help the user."
+            ),
+            verbose=False,
+        )
     def chat_function(self, query):
         """ Chat function to query the index."""
-        response = self.engine.query(query).response
+        response = self.engine.chat(query).response
         result = {'answer': response}
         return result
